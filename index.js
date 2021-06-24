df75b4f473b060692e9cebf256da0f7f95096270
stdavis <stdavis@utah.gov>
2021-06-24 13:21:56 -0600

    feat: add repo/owner inputs

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const LABEL = 'reminder';
 
 function createComment(octokit, context, body) {
   return octokit.rest.issues.createComment({
-    owner: 'agrc',
-    repo: 'reminder-action',
+    owner: context.repository.owner,
+    repo: context.repository.name,
     issue_number: context.issue.number,
     body
   });
@@ -16,7 +16,7 @@ function updateIssue(octokit, context, reminder) {
   const body = addReminderToBody(context.issue.body, reminder);
 
   return octokit.rest.issues.update({
-    owner: context.repository.owner.id,
+    owner: context.repository.owner,
     repo: context.repository.name,
     issue_number: context.issue.number,
     body
@@ -25,33 +25,52 @@ function updateIssue(octokit, context, reminder) {
 
 async function run() {
   const context = github.context.payload;
+  const owner = core.getInput('repository_owner');
+  const repository = core.getInput('repository');
+  context.repository = {
+    owner,
+    name: repository
+  };
   const octokit = github.getOctokit(core.getInput('repoToken', {required:true}));
   let reminder;
 
   try {
+    core.startGroup('parsing reminder');
     reminder = getReminder(context);
+
+    core.info(JSON.stringify(reminder, null, 1));
 
     if (!reminder) {
       return;
     }
+    core.endGroup();
 
   } catch (error) {
+    core.startGroup('create error comment');
     await createComment(octokit, context, `@${context.sender.login} we had trouble parsing your reminder. Try:\n\n\`/remind me [what] [when]\``);
+    core.endGroup();
+
     core.setFailed(error);
 
     return;
   }
 
+  core.startGroup('add label');
   await octokit.rest.issues.addLabels({
     owner: context.repository.owner.id,
     repo: context.repository.name,
     issue_number: context.issue.number,
     labels: LABEL
   });
+  core.endGroup();
 
-  updateIssue(octokit, context, reminder);
+  core.startGroup('update issue');
+  await updateIssue(octokit, context, reminder);
+  core.endGroup();
 
+  core.startGroup('add reminder comment');
   await createComment(octokit, context, `@${context.sender.login} set a reminder for **${reminder.when.toLocaleDateString()}**`);
+  core.endGroup();
 }
 
 run();
