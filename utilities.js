d327052db4d4db77e0bfd7373a430e6b559c7455
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2021-08-26 13:06:47 +0300

    Increase flexibility of /remind statement (fixes #14)

--- utilities.js
+++ utilities.js
@@ -5,16 +5,47 @@ const parseReminder = require('parse-reminder');
 //   when: 2017-09-12T12:00:00.000Z }
 function getReminder(context, referenceDate = null) {
   const body = context.comment.body;
-  if (!body.startsWith('/')) {
+  var remindLine = null;
+  var inCode = false;
+
+  const lines = body.split('\n');
+  for (let i=0; i<lines.length; i++) {
+    var line = lines[i].trim('\r');
+
+    // handle code blocks
+    if (line.startsWith('```')) {
+      inCode = !inCode;
+      continue;
+    }
+    if (inCode)
+      continue;
+
+    // handle quoted text
+    if (line.trim(' ').startsWith('>')) {
+      continue;
+    }
+
+    // find /remind in line.
+    var words = line.split(' ');
+    var foundRemind = false;
+    for (let j=0; j<words.length; j++) {
+      var word = words[j];
+      if (word === '/remind') {
+        foundRemind = true;
+        remindLine = words.slice(j).join(' ');
+        break;
+      }
+    }
+    if (foundRemind) {
+      break;
+    }
+  }
+
+  if (remindLine === null) {
     return null;
   }
 
-  const firstWord = body.slice(1, body.indexOf(' '));
-  if (firstWord !== 'remind') {
-    return null;
-  }
-
-  const reminder = parseReminder(body.slice(1), referenceDate);
+  const reminder = parseReminder(remindLine.slice(1), referenceDate);
 
   if (!reminder) {
     throw new Error(`Unable to parse reminder: remind ${body}`);
